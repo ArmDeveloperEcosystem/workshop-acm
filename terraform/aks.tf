f5a55a5eed24b5360979e299e79c1e63f4c5da80
Avin Zarlez <4392252+AvinZarlez@users.noreply.github.com>
2025-06-13 13:32:43 -0700

    Fix AKS

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -9,8 +9,11 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
   default_node_pool {
     name       = "armpool"
-    vm_size    = "Standard_D2ps_v6"
+    vm_size    = "Standard_D16ps_v6"
     node_count = var.agent_count
+    os_disk_size_gb = 64
+    temporary_name_for_rotation = "armpooltemp"
+
   }
   identity {
     type = "SystemAssigned"
@@ -20,8 +23,10 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 resource "azurerm_kubernetes_cluster_node_pool" "amdcluster" {
   name                  = "amdpool"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.k8s.id
-  vm_size               = "Standard_D2as_v5"
+  vm_size               = "Standard_D16as_v5"
   node_count            = var.agent_count
+  os_disk_size_gb = 64
+  temporary_name_for_rotation = "amdpooltemp"
 
   tags = {
     Environment = "Demo"
