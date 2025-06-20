617010963f71359a7cb703122c2b4cacf8319cca
Avin Zarlez <4392252+AvinZarlez@users.noreply.github.com>
2025-06-20 15:16:24 -0700

    static ip

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -19,7 +19,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.vmrg.location
   resource_group_name = azurerm_resource_group.vmrg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Create Network Security Group and rule
