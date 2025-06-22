98eb672f99887680a4262472db29d5984a7a730b
Avin Zarlez <4392252+AvinZarlez@users.noreply.github.com>
2025-06-22 13:16:42 -0700

    Fixed check check script

--- instruqt/1-terraform/check-cloud-client.sh
+++ instruqt/1-terraform/check-cloud-client.sh
@@ -41,10 +41,10 @@ check_resource() {
             az acr show --name "$resource_name" --subscription "$SUBSCRIPTION_ID" &>/dev/null
             ;;
         nodePool)
-            az aks nodepool show --cluster-name "${EXPECTED_RESOURCES[1]#*:}" --resource-group "${EXPECTED_RESOURCES[0]#*:}" --name "$resource_name" --subscription "$SUBSCRIPTION_ID" &>/dev/null
+            az aks nodepool show --cluster-name "${EXPECTED_RESOURCES[2]#*:}" --resource-group "${EXPECTED_RESOURCES[0]#*:}" --name "$resource_name" --subscription "$SUBSCRIPTION_ID" &>/dev/null
             ;;
         vm)
-            az vm show --name "$resource_name" --resource-group "${EXPECTED_RESOURCES[0]#*:}" --subscription "$SUBSCRIPTION_ID" &>/dev/null
+            az vm show --name "$resource_name" --resource-group "${EXPECTED_RESOURCES[1]#*:}" --subscription "$SUBSCRIPTION_ID" &>/dev/null
             ;;
         *)
             echo "Unknown resource type: $resource_type"
