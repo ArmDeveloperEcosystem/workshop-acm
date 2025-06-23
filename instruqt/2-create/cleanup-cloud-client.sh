f830fc73b31dfb09994930eb2ee407e49861eeed
Avin Zarlez <4392252+AvinZarlez@users.noreply.github.com>
2025-06-23 12:22:07 -0700

    deallocate

--- instruqt/2-create/cleanup-cloud-client.sh
+++ instruqt/2-create/cleanup-cloud-client.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 UNIQUE_ID=$(agent variable get randomid)
 
 # Stop the VM
-az vm stop --name workshop-vm --resource-group workshop-demo-rg-$UNIQUE_ID-vm
+az vm deallocate --name workshop-vm --resource-group workshop-demo-rg-$UNIQUE_ID-vm
